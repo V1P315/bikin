78c2ec87fdb4c2b28836aee3f6267dcc37ba8a80
V1P315 <abderios32@gmail.com>
2025-09-04 17:41:10 +0700

    Create pengenelanCPP.cpp

---
+++ pengenelanCPP.cpp
@@ -0,0 +1,18 @@
+#include <studio.h>
+#include <string>
+using namespace std;
+
+int main() {
+    string nama, npm, kelas;
+
+    cout << "Masukkan Nama: ";
+    cin >> nama;
+
+    cout << "Masukkan NPM: ";
+    cin >> npm;
+
+    cout << "Masukkan Kelas: ";
+    cin >> kelas;
+
+    return 0;
+}
